2c8f45db6a50ab4c8b4bba99d26ad37c58163e78
alex <kondratenkoalex1994@gmail.com>
2022-12-13 11:39:25 +0300

    fix update luasm mistake

--- luasm.sh
+++ luasm.sh
@@ -3,34 +3,35 @@ source ./lib
 set -e
 
 luasm_update() {
-    if [ "$(is_new_version ${LUASM_DIR})" -eq "1" ]; then
-        echo -en "\n    > ${YELLOW}Update LUASM software to latest version? (${GREEN}Y${NORMAL}/${RED}N${YELLOW}): "
-        read yn
-            case "${yn}" in
-        y|Y|yes)
-            cd /${HOME}/${LUASM_DIR}
-            git pull
-            clear
-            echo -en "     ${GREEN}LUASM software successfully updated! \n"
-            go_to_screen main;;  
-        n|N|no)
-            choise "back" "main";;
-        *)
-            choise "error" "start_app";;
-        esac
-    else
-        go_to_screen main
-    fi
+    echo -en "\n    > ${YELLOW}Update LUASM software to latest version? (${GREEN}Y${NORMAL}/${RED}N${YELLOW}): "
+    read yn
+        case "${yn}" in
+    y|Y|yes)
+        cd /${HOME}/${LUASM_DIR}
+        git pull
+        clear
+        echo -en "     ${GREEN}LUASM software successfully updated! \n"
+        go_to_screen main;;  
+    n|N|no)
+        choise "back" "main";;
+    *)
+        clear
+        choise "error" "start_app";;
+    esac
 }
 
 start_app() {
-    clear
-    menu_head
     if [ "$(check_connection)" -eq "1" ]; then
-        luasm_update
+        if [ "$(is_new_version ${LUASM_DIR})" -eq "0" ]; then
+            luasm_update
+        else
+            go_to_screen main
+        fi
     else
         go_to_screen main
     fi
 }
 
+clear
+menu_head
 start_app
